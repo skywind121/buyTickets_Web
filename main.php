b3bf585e215e70eb4694232086722e3b6e8376be
Yellow-Shadow <58462821+Yellow-Shadow@users.noreply.github.com>
2022-01-15 21:01:42 +0800

    Update main.php

--- main.php
+++ main.php
@@ -6,21 +6,28 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
         <a href="http://localhost/main.php" style="text-decoration:none;">
             <img src="/img/ticket.png" width="20%" height="20%">
             <font color="white" size="6"><b>海大資工購票網</b></font>
         </a>
+        
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">首頁</a>
+            <a class="navbar-brand" href="http://localhost/main.php"><b>首頁</b></a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
+            <a class="navbar-brand" href="http://localhost/News.php"><b>公告</b></a>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="http://localhost/news.php">
+                        <a class="navbar-brand" aria-current="page" href="http://localhost/news.php">
                             公告
                         </a>
                     </li>
@@ -39,9 +46,6 @@
                             </li>
                             <li><a class="dropdown-item" href="#">Something else here</a></li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
                     </li>
                 </ul>
                 <form class="d-flex">
@@ -66,4 +70,5 @@
     <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
