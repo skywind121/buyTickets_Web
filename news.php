bac80fce10b48b1c5859262e4fe741c308f0d6ae
skywind121 <46217270+skywind121@users.noreply.github.com>
2022-01-15 19:34:50 +0800

    Update news.php

--- news.php
+++ news.php
@@ -5,9 +5,14 @@
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        
+        <style type = "text/CSS">
+		.table {
+			width: 500px;
+		}
+	</style>
     </head>
 
- 
     <body>
 
         <a href="http://localhost/main.php" style="text-decoration:none;">
@@ -20,18 +25,13 @@
         </a>
         <br><br>
 
-        <table class="table table-hover">
-
-        </table>
-
         <?php
-
         include "db_conn.php";
 
         echo "
-        <table border = '1'>
-        <tr>
-        <th>姓名</th>
+        <table border = '1' class='table table-bordered'>
+        <tr width='10px'>
+        <th style='width:30%'>姓名</th>
         <th>年齡</th>
         <th>性別</th>
         <th>號碼</th>
