6bfa9a6d1f8e98c8138598e7a87e8fa8d6957494
skywind121 <46217270+skywind121@users.noreply.github.com>
2022-01-15 20:30:50 +0800

    0.4

--- main.php
+++ main.php
@@ -3,15 +3,15 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html" ; charset="utf-8">
     <title>海大資工售票網</title>
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-
-
 <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a href="http://localhost/main.php" style="text-decoration:none;">
+            <img src="/img/ticket.png" width="20%" height="20%">
+            <font color="white" size="6"><b>海大資工購票網</b></font>
+        </a>
         <div class="container-fluid">
             <a class="navbar-brand" href="#">首頁</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +20,9 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">公告</a>
+                        <a class="nav-link active" aria-current="page" href="http://localhost/news.php">
+                            公告
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">登入</a>
@@ -49,16 +51,6 @@
             </div>
         </div>
     </nav>
-
-    <a href="http://localhost/main.php" style="text-decoration:none;">
-        <img src="/img/ticket.png" width="5%" height="10%">
-        <font color="blue" size="6"><b>海大資工購票網</b></font>
-    </a>
-    |
-    <a href="http://localhost/news.php" style="text-decoration:none;">
-        <font color="black" size="6">公告</font>
-    </a>
-
     <form action="ininsert.php" method="post">
         <br><br>
         姓名:<input type="varchar" name="name">
@@ -71,9 +63,7 @@
         <br><br>
         <input type="submit" name="up" style="background-color#5393e8" value="送出">
     </form>
-
     <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- news.php
+++ news.php
@@ -17,14 +17,50 @@
  
     <body>
 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a href="http://localhost/main.php" style="text-decoration:none;">
-            <img src="/img/ticket.png" width="5%" height="10%" >
-            <font color="black" size="6" >海大資工購票網</font>
+            <img src="/img/ticket.png" width="20%" height="20%">
+            <font color="white" size="6"><b>海大資工購票網</b></font>
         </a>
-        |
-        <a href="http://localhost/news.php" style="text-decoration:none;">
-            <font color="blue" size="6" ><b>公告</b></font>
-        </a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="http://localhost/main.php">首頁</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="http://localhost/news.php">
+                            公告
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://localhost/login.php">登入</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            節目資訊
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
         <br><br>
 
         <?php
