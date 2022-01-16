3d737e4fc7ab67a769d49cf9a41cf2160fd035e0
Yellow-Shadow <58462821+Yellow-Shadow@users.noreply.github.com>
2022-01-16 15:25:25 +0800

    icon2

--- news.php
+++ news.php
@@ -1,21 +1,4 @@
 <html>
-<<<<<<< HEAD
-
-<head>
-
-    <meta http-equiv="Content-Type" content="text/html" ; charset="utf-8">
-    <title>公告</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <!-- Option 1: Include in HTML -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
-    <style type = "text/CSS">
-		p{
-            font-size: 100px;
-=======
     <head>
         <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
         <title>公告</title>
@@ -66,7 +49,6 @@
                 echo "<td>".$result->no."</td>";
                 echo "</tr>";
             }
->>>>>>> 7ebd737b5a7dacbfd24be18dec6f7bfd2094e27f
         }
 	</style>
 
