f2bceac6ff0d45587b75ccccec20cf8bc650bcfc
skywind121 <46217270+skywind121@users.noreply.github.com>
2022-01-16 16:27:36 +0800

    1.9

--- delEvent.php
+++ delEvent.php
@@ -1,10 +1,10 @@
 <?php
   include('db_conn.php');
 
-  $id = $_GET['id'];
+  
   $stmt = $db->prepare('delete from events where eId=?');
   $stmt -> bind_param("i", $id);
-  
+  $id = $_GET['id'];
   $stmt -> execute();
 
   // 如果刪除成功
--- ininsert.php
+++ ininsert.php
@@ -1,54 +1,14 @@
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
-        <title>HelloWorld</title>
-        <style type = "text/CSS">
-            body{
-                text-align: center;
-            }
-        </style>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
+<?php
+include "db_conn.php";
 
-    <body>
+$eventname   = $_POST["eventname"];
+$ticketPrice    = $_POST["ticketPrice"];
+$numTickets = $_POST["numTickets"];
+$eId     = $_POST["eId"];
 
-        <?php include "topButton.php"; ?>
-        <br><br>
-        <?php
-        include "db_conn.php";
-
-        $name   = $_POST["name"];
-        $ticketPrice    = $_POST["ticketPrice"];
-        $numTickets = $_POST["numTickets"];
-        $eId     = $_POST["eId"];
-
-        $query = ("insert into events values(?,?,?,?)");
-        $stmt = $db -> prepare($query);
-        $stmt -> bind_param("siii", $name,$ticketPrice,$numTickets,$eId);
-        $stmt -> execute();
-
-        echo "
-        <table border = '1'>
-        <tr>
-        <th>活動名稱</th>
-        <th>門票價錢</th>
-        <th>門票數量</th>
-        <th>活動編號</th>
-        </tr>";
-        $query2 = "SELECT * FROM events";
-        if($stmt = $db -> query($query2)){
-            while($result = mysqli_fetch_object($stmt)){
-                echo "<tr>";
-                echo "<td>".$result->name."</td>";
-                echo "<td>".$result->ticketPrice."</td>";
-                echo "<td>".$result->numTickets."</td>";
-                echo "<td>".$result->eId."</td>";
-                echo "</tr>";
-            }
-        }
-        echo "</table>";
-        ?>
-
-        <a href="http://localhost/main.php">返回</a>
-    </body>
-</html>
+$query = ("insert into events values(?,?,?,?)");
+$stmt = $db -> prepare($query);
+$stmt -> bind_param("siii", $eventname,$ticketPrice,$numTickets,$eId);
+$stmt -> execute();
+header("Location: listEvents.php");
+?>
