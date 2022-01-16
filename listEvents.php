9fe64ea8c41de5aeabb0e25dfb52865a4adfe5ed
golddragon11 <golddragon11@gmail.com>
2022-01-16 15:55:51 +0800

    Revert

--- listEvents.php
+++ listEvents.php
@@ -1,56 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+   
+	<head>
+		<title>Events</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	</head>
 
-<head>
-	<title>Events</title>
+	<style>
+		tr,td{
+			width: 5px;
+		}
+	</style>
 
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<body>
 
-	<!-- Option 1: Include in HTML -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
-</head>
-
-<style>
-	tr,
-	td {
-		width: 5px;
-	}
-</style>
-
-<body>
-
-	<?php
-	include "topButton.php";
-	$query = "SELECT * FROM events";
-	if ($stmt = $db->query($query)) {
-		$result = $stmt->fetch_all();
-	}
+	<?php 
+		include "topButton.php"; 
+		$query = "SELECT * FROM events";
+		if ($stmt = $db->query($query)) {
+			$result = $stmt->fetch_all();
+		}
 	?>
 		
 		<br><br>
 		<h1>活動列表</h1>
 		<table class="table table-striped">
 			<tr>
-				<td style="text-align:center" >活動名稱</td>
-				<td style="text-align:center">門票價錢</td>
-				<td style="text-align:center">門票數量</td>
-				<td style="text-align:center">活動編號</td>
+				<td style="text-align:center">Event Name</td>
+				<td style="text-align:center">Ticket Price</td>
+				<td style="text-align:center">Number of Tickets</td>
 			</tr>
 		<?php
 			foreach ($result as $row) {
-				echo "<tr>
-				<td style='text-align:center'>".$row[0]."</td>
-				<td style='text-align:center'>".$row[1]."</td>
-				<td style='text-align:center'>".$row[2]."</td>
-				<td style='text-align:center'>".$row[3]."</td>
-				<td style='text-align:center'>"."修改"."</td>
-				<td><a href='delEvent.php?eId='.$row[3].''>刪除</a></td>
-				<tr>";
+				echo "<tr><td style='text-align:center'>".$row[0]."</td><td style='text-align:center'>".$row[1]."</td><td style='text-align:center'>".$row[2]."</td><tr>";
 			}
 		?>
-	</table>
-</body>
-
+		</table>
+	</body>
+   
 </html>
